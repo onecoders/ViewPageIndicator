18bbd578ddbf9573e02fdb89a82faa850f5be597
silicon <onecodes@gmail.com>
2013-10-17 10:34:03 +0800

    network connection utils

--- CompIntro/src/com/intro/compintro/util/Utils.java
+++ CompIntro/src/com/intro/compintro/util/Utils.java
@@ -21,4 +21,49 @@ public class Utils {
 		return false;
 	}
 
+	public static boolean isWifiConnected(Context context) {
+		if (context != null) {
+			ConnectivityManager mConnecttivityManager = (ConnectivityManager) context
+					.getSystemService(Context.CONNECTIVITY_SERVICE);
+			if (mConnecttivityManager != null) {
+				NetworkInfo mWifiNetworkInfo = mConnecttivityManager
+						.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
+				if (mWifiNetworkInfo != null) {
+					return mWifiNetworkInfo.isAvailable();
+				}
+			}
+		}
+		return false;
+	}
+
+	public static boolean isMobileConnected(Context context) {
+		if (context != null) {
+			ConnectivityManager mConnecttivityManager = (ConnectivityManager) context
+					.getSystemService(Context.CONNECTIVITY_SERVICE);
+			if (mConnecttivityManager != null) {
+				NetworkInfo mMobileNetworkInfo = mConnecttivityManager
+						.getNetworkInfo(ConnectivityManager.TYPE_MOBILE);
+				if (mMobileNetworkInfo != null) {
+					return mMobileNetworkInfo.isAvailable();
+				}
+			}
+		}
+		return false;
+	}
+
+	public static int getConnectedType(Context context) {
+		if (context != null) {
+			ConnectivityManager mConnecttivityManager = (ConnectivityManager) context
+					.getSystemService(Context.CONNECTIVITY_SERVICE);
+			if (mConnecttivityManager != null) {
+				NetworkInfo mNetworkInfo = mConnecttivityManager
+						.getActiveNetworkInfo();
+				if (mNetworkInfo != null && mNetworkInfo.isAvailable()) {
+					return mNetworkInfo.getType();
+				}
+			}
+		}
+		return -1;
+	}
+
 }
