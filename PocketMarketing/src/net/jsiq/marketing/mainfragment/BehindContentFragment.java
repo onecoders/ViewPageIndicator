cb2c150242ea54df59ed0008a0baa2a523982669
onecoders@gmail.com <shenlv40432679>
2013-11-12 13:51:09 +0800

    behindContent add progressbardialog

--- PocketMarketing/src/net/jsiq/marketing/mainfragment/BehindContentFragment.java
+++ PocketMarketing/src/net/jsiq/marketing/mainfragment/BehindContentFragment.java
@@ -1,5 +1,6 @@
 package net.jsiq.marketing.mainfragment;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import net.jsiq.marketing.R;
@@ -8,44 +9,71 @@ import net.jsiq.marketing.adapter.BehindMenuAdapter;
 import net.jsiq.marketing.constants.URLStrings;
 import net.jsiq.marketing.model.MenuItem;
 import net.jsiq.marketing.util.LoaderUtil;
+import net.jsiq.marketing.util.MessageToast;
+import net.jsiq.marketing.util.NetworkUtils;
+import android.app.ProgressDialog;
+import android.content.Context;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.actionbarsherlock.app.SherlockFragment;
 import com.actionbarsherlock.app.SherlockListFragment;
 
 public class BehindContentFragment extends SherlockListFragment {
-
+	
 	private BehindMenuAdapter adapter;
+	private List<MenuItem> menuList;
+	private Context context;
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
-		return inflater.inflate(R.layout.behind_menu, null);
+		this.context = getSherlockActivity();
+		View behindContentView = inflater.inflate(R.layout.behind_menu, null);
+		return behindContentView;
 	}
-
+	
 	@Override
 	public void onActivityCreated(Bundle savedInstanceState) {
 		super.onActivityCreated(savedInstanceState);
-		new InitMenuAsyncTask().execute(URLStrings.GET_MENUS);
+		if (NetworkUtils.isNetworkConnected(context)) {
+			new InitMenuAsyncTask().execute(URLStrings.GET_MENUS);
+		} else {
+			MessageToast.makeText(context, R.string.notConnected,
+					Toast.LENGTH_SHORT).show();
+		}
 	}
 
-	class InitMenuAsyncTask extends AsyncTask<String, Void, List<MenuItem>> {
+	class InitMenuAsyncTask extends AsyncTask<String, Void, Void> {
 
+		private ProgressDialog dialog;
+		
 		@Override
-		protected List<MenuItem> doInBackground(String... params) {
-			return LoaderUtil.loadMenuItems(params[0]);
+		protected void onPreExecute() {
+			super.onPreExecute();
+			menuList = new ArrayList<MenuItem>();
+			dialog = new ProgressDialog(context);
+			dialog.setMessage("内容导入中...");
+			dialog.show();
 		}
 
 		@Override
-		protected void onPostExecute(List<MenuItem> result) {
+		protected Void doInBackground(String... params) {
+			menuList = LoaderUtil.loadMenuItems(params[0]);
+			return null;
+		}
+
+		@Override
+		protected void onPostExecute(Void result) {
 			super.onPostExecute(result);
-			adapter = new BehindMenuAdapter(getSherlockActivity(), result);
+			adapter = new BehindMenuAdapter(context, menuList);
 			setListAdapter(adapter);
+			dialog.dismiss();
 		}
 
 	}
