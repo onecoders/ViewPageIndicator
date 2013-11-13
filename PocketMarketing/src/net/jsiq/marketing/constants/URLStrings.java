71ffcc20d9a75ceca584219fe93d50656b9ccd06
onecoders@gmail.com <shenlv40432679>
2013-11-13 11:40:47 +0800

    barcodeScan intergrated to app

--- PocketMarketing/src/net/jsiq/marketing/constants/URLStrings.java
+++ PocketMarketing/src/net/jsiq/marketing/constants/URLStrings.java
@@ -2,12 +2,17 @@ package net.jsiq.marketing.constants;
 
 public class URLStrings {
 
-	public static final String GET_MENUS = "http://www.jsiq.net:6060/mpms/clientService/getMenus";
+	private static final String BASIC_URL = "http://www.jsiq.net:6060/mpms/clientService/";
 
-	public static final String GET_CATALOGS_BY_MENUID = "http://www.jsiq.net:6060/mpms/clientService/getCatalogs/";
+	public static final String GET_MENUS = BASIC_URL + "getMenus";
 
-	public static final String GET_CONTENTS_BY_CATALOGID_PAGENO = "http://www.jsiq.net:6060/mpms/clientService/getContents/";
+	public static final String GET_CATALOGS_BY_MENUID = BASIC_URL
+			+ "getCatalogs/";
 
-	public static final String GET_CONTENT_BY_CONTENT_ID = "http://www.jsiq.net:6060/mpms/clientService/viewContent/";
+	public static final String GET_CONTENTS_BY_CATALOGID_PAGENO = BASIC_URL
+			+ "getContents/";
+
+	public static final String GET_CONTENT_BY_CONTENT_ID = BASIC_URL
+			+ "viewContent/";
 
 }
